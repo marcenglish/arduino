a2da378d21afbac688a240b6816575727228b726
marcenglish <idounderstand@gmail.com>
2017-09-25 23:00:27 -0700

    Adding goal state change detection logic.

--- goal_buzzer/goal_buzzer.ino
+++ goal_buzzer/goal_buzzer.ino
@@ -4,15 +4,23 @@
 #include <ArduinoJson.h>
 
 //------- Replace the following! ------
-char ssid[] = "##";       // your network SSID (name)
-char password[] = "##";  // your network key
-char* test = "";
+char ssid[] = "#";       // your network SSID (name)
+char password[] = "#";  // your network key
+String team = "Florida";
+String team_type = "None";
 char* feed_url = "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812";
+
+String data;
+bool game_started = false;
+int interval = 10000;
+int current_game = 0;
+int team_goals = 1;
+int current_goals = 1;
 WiFiClientSecure client;
-//String data = "None";
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   //Setup WIFI
   WiFi.mode(WIFI_STA);
@@ -31,7 +39,7 @@ void setup() {
 }
 
 void loop() {
-  String data = http_get(feed_url);
+  data = http_get(feed_url);
   Serial.println("Go...");
   //JSON Parsing Prep
   //Shave start
@@ -49,37 +57,63 @@ void loop() {
       //Setup JSON
       StaticJsonBuffer<1000> jsonBuffer;  
       JsonObject& game_result = jsonBuffer.parseObject(test_data);
+
       String away_team = game_result["atn"];
       String home_team = game_result["htn"];
-     
-      Serial.println(away_team);
-      Serial.println(home_team);
-      Serial.println("---");
-      data = data.substring(end_index+2);
+      String game_status = game_result["bs"];
 
-  }
-    
-  
-//  Serial.println("-----");
-//  String game_result["Date"] = data["ts"];
-//  Serial.println(game_result["Date"]);
-//  String game_result["Status"] = data["bs"];
-//  Serial.println(game_result["Status"]);
-//  String game_result["Away Team"] = data["atn"];
-//  Serial.println(game_result["Away Team"]);
-//  String game_result["Away Goals"] = data["ats"];
-//  Serial.println(game_result["Away Goals"]);
-//  String game_result["Home Team"] = data["htn"];
-//  Serial.println(game_result["Home Team"]);
-//  String game_result["Home Goals"] = data["hts"];
-//  Serial.println(game_result["Home Goals"]);
-  delay(10000);
-  
+      //A given team may have previous and upcoming games listed.  
+      //Get the game id of the live game
+      if (game_started != true) {
+        if ((away_team == team) || (home_team == team)){
+          if (game_status == "LIVE"){
+            Serial.println("game is on now!!");
+            current_game = game_result["id"];
+            game_started = true;
+            if (away_team == team){
+              team_type = "away";
+            }
+            if (home_team == team){
+              team_type = "home";
+            }
+          }else if (game_status != "FINAL"){
+            Serial.println("waiting for game to start");
+          }
+        }
+      }
+
+      //
+      if ((away_team == team) || (home_team == team)){
+//      if (current_game == game_result["id"]){
+        String away_team_goals = game_result["ats"];
+        String home_team_goals = game_result["hts"];
+        Serial.println(game_status);
+        Serial.println(away_team);
+        Serial.println(away_team_goals);
+        Serial.println(home_team);
+        Serial.println(home_team_goals);
+        Serial.println("---");
+//        if (team_type == "away"){
+//          current_goals = game_result["ats"].toInt();          
+//        }
+//        if (team_type == "home"){
+//          current_goals = game_result["hts"].toInt(); 
+//        }
+//        if (current_goals > team_goals){
+//          announce_goal();
+//          team_goals = current_goals;
+//        }
+      }
+      data = data.substring(end_index+2);
+  }  
+  delay(interval);  
 }
 
+void announce_goal(){
+  Serial.println("Goal scored!");
+}
 
-
-String http_get(String url){;
+String http_get(String url){
   HTTPClient http;
   http.begin(url);
   int httpCode = http.GET();
