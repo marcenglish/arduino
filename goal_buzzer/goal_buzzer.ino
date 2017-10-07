3bb033e8dd2a57cecb51a3f837eeb42b9cb2c360
marcenglish <idounderstand@gmail.com>
2017-10-06 23:47:42 -0700

    added lcd display and runtime team selection

--- goal_buzzer/goal_buzzer.ino
+++ goal_buzzer/goal_buzzer.ino
@@ -2,17 +2,24 @@
 #include <WiFiClientSecure.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const int buttonPin = 14;     // the number of the pushbutton pin
-const int wifi_led_pin = 5;
-const int islive_pin = 12;
-#define buzzer 4
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int wifi_led_pin = 15;
+const int islive_pin = 13;
+const int pot_pin = 0;
+int prev_sensor_value = 0;
+#define buzzer 14
+
+#include <LiquidCrystal_I2C.h>  
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 //------- Replace the following! ------
 char ssid[] = "#";       // your network SSID (name)
 char password[] = "#";  // your network key
 char* feed_url = "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812";
 
-String team = "San Jose";
+String team = "";
 String data;
 int buttonState = 0;         // variable for reading the pushbutton status
 bool game_started = false;
@@ -22,36 +29,68 @@ int team_goals = 0;
 int opponent_goals = 0;
 int spread = 0;
 int spread_prev = 0;
-
+String teams[] = {"Anaheim", "Arizona", "Boston", "Buffalo", "Calgary", "Carolina", "Chicago", 
+                  "Colorado", "Columbus", "Dallas", "Detroit", "Edmonton", "Florida", "Los Angeles", 
+                  "Minnesota", "Montreal", "Nashville", "New Jersey", "NY Rangers", "NY Islanders", 
+                  "Ottawa", "Philadelphia", "Pittsburgh", "San Jose", "St. Louis", 
+                  "Tampa Bay", "Toronto", "Vancouver", "Vegas", "Washington", "Winnipeg"};
 
 WiFiClientSecure client;
 
 void setup() {
   Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+
   pinMode(buttonPin, INPUT);
   pinMode(wifi_led_pin, OUTPUT);
   pinMode(islive_pin, OUTPUT);
+  
   //Setup WIFI
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   WiFi.begin(ssid, password);
+  display("Connecting to ",0);
+  display(ssid, 1);
+  
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(100);
   }
   IPAddress ip = WiFi.localIP();
-  delay(100);
-  Serial.println("Connected!");
-  delay(100);
-  Serial.println(ip);
+  display("Connected!", 2);
+  display(WiFi.localIP().toString(), 3);
+  delay(1500);
+  display_clear();
+
+
   
 }
 
 void loop() {
-//  buttonState = digitalRead(buttonPin);
-//  if (buttonState == HIGH) {
-//    announce_goal(); 
-//  }
+  buttonState = digitalRead(buttonPin);
+
+  //Initialize the thing.  Prompt user for team selection.
+  if (team == ""){
+    display("Please select a team", 0);
+    int sensor_value = analogRead(pot_pin);
+    sensor_value = map(sensor_value, 0, 1023, 0, 30);
+    if (sensor_value != prev_sensor_value){
+        display("                    ", 1);
+        display(teams[sensor_value],1);
+        prev_sensor_value = sensor_value;
+    }
+    if (buttonState == HIGH) {
+      team = teams[sensor_value];
+      display_clear();
+      delay(1000);
+    }
+  }else{
+    display("Team is:", 0);
+    display(team, 1);
+    if (buttonState == HIGH) {
+        announce_goal(); 
+    }
 
   check_wifi();
   
@@ -93,8 +132,11 @@ void loop() {
             current_game_id = game_result["id"];
             game_started = true;
             announce_game_start();
-          }else if ((game_status != "FINAL") && (game_status != "FINAL OT") && (game_status != "FINAL SO")) {
-            Serial.println("waiting for game to start");
+            display_clear();
+          //}else if ((game_status != "FINAL") && (game_status != "FINAL OT") && (game_status != "FINAL SO")) {
+          }else{
+            display("                    ",2);
+            display("waiting to start", 3);
           }
         }
       }
@@ -111,8 +153,8 @@ void loop() {
           
           spread = (team_goals - opponent_goals);
           
-          Serial.println("Spread is: ");
-          Serial.println(spread);
+          display("Spread is: ", 2);
+          display(String(spread), 3);
   
           if (spread > spread_prev){
             announce_goal();
@@ -133,40 +175,40 @@ void loop() {
   }  
 
   delay(interval);  
-  
+  }
 }
 
 void announce_game_start(){
   Serial.println("Goal scored!");
-  tone(buzzer, 100, 1000);
-  tone(buzzer, 200, 1000);
+ tone(buzzer, 100, 1000);
+ tone(buzzer, 200, 1000);
   
 }
 
 void announce_goal(){
   Serial.println("Goal scored!");
-  tone(buzzer, 100, 100);
-  delay(200);
-  tone(buzzer, 100, 100);
-  delay(200);
-  tone(buzzer, 100, 100);
-  delay(200);
-  tone(buzzer, 200, 300);
-  delay(200);    
+ tone(buzzer, 100, 100);
+ delay(200);
+ tone(buzzer, 100, 100);
+ delay(200);
+ tone(buzzer, 100, 100);
+ delay(200);
+ tone(buzzer, 200, 300);
+ delay(200);    
 }
 
 void announce_win(){
   Serial.println("We won!");
-  tone(buzzer, 300, 100);
-  delay(200);
-  tone(buzzer, 100, 100);
-  delay(200);
-  tone(buzzer, 300, 100);
-  delay(200);
-  tone(buzzer, 200, 300);
-  delay(200);    
-  tone(buzzer, 300, 600);
-  delay(200);
+ tone(buzzer, 300, 100);
+ delay(200);
+ tone(buzzer, 100, 100);
+ delay(200);
+ tone(buzzer, 300, 100);
+ delay(200);
+ tone(buzzer, 200, 300);
+ delay(200);    
+ tone(buzzer, 300, 600);
+ delay(200);
 }
 
 String http_get(String url){
@@ -193,4 +235,23 @@ void check_islive(){
     digitalWrite(islive_pin, LOW);
 
   }
+}
+
+
+void display(String chars, int line){
+    // Serial.println(chars);
+    // Serial.println(line);
+    
+    // lcd.setCursor(0,line);
+    // lcd.print("                    ");
+    lcd.setCursor(0,line);
+    lcd.print(chars);
+
+}
+
+void display_clear(){
+    for (int i=0; i < 5; i++){
+        lcd.setCursor(0,i);
+        lcd.print("                    ");    
+    }
 }
