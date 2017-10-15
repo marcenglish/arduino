334de9db23ddd20701672fba43b468cc21f37ea3
marcenglish <idounderstand@gmail.com>
2017-10-14 23:51:53 -0700

    game data sort process simplified

--- goal_buzzer/goal_buzzer.ino
+++ goal_buzzer/goal_buzzer.ino
@@ -19,16 +19,18 @@ char ssid[] = "#";       // your network SSID (name)
 char password[] = "#";  // your network key
 char* feed_url = "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812";
 
-String team = "";
+String team = "Boston";
 String data;
+String gamedata;
 int buttonState = 0;         // variable for reading the pushbutton status
 bool game_started = false;
 int interval = 10000;
 int current_game_id = 0;
 int team_goals = 0;
 int opponent_goals = 0;
-int spread = 0;
+String spread = "None";
 int spread_prev = 0;
+long previousMillis = 0; 
 String teams[] = {"Anaheim", "Arizona", "Boston", "Buffalo", "Calgary", "Carolina", "Chicago", 
                   "Colorado", "Columbus", "Dallas", "Detroit", "Edmonton", "Florida", "Los Angeles", 
                   "Minnesota", "Montreal", "Nashville", "New Jersey", "NY Rangers", "NY Islanders", 
@@ -36,11 +38,11 @@ String teams[] = {"Anaheim", "Arizona", "Boston", "Buffalo", "Calgary", "Carolin
                   "Tampa Bay", "Toronto", "Vancouver", "Vegas", "Washington", "Winnipeg"};
 
 WiFiClientSecure client;
-
+int count = 0;
 void setup() {
   Serial.begin(115200);
   lcd.init();
-  lcd.backlight();
+  lcd.backlight();  
 
   pinMode(buttonPin, INPUT);
   pinMode(wifi_led_pin, OUTPUT);
@@ -62,55 +64,67 @@ void setup() {
   display(WiFi.localIP().toString(), 3);
   delay(1500);
   display_clear();
+  
 
-  announce_goal();
+  announce_start();
   
 }
 
 void loop() {
-    buttonState = digitalRead(buttonPin);
-    lcd.setCursor(19,0);
-    lcd.print(buttonState);
-    //Initialize the thing.  Prompt user for team selection.
-    if (team == ""){
-        display("Please select a team", 0);
-        int sensor_value = analogRead(pot_pin);
-        sensor_value = map(sensor_value, 0, 1023, 0, 30);
+    delay(100);
 
-        if (sensor_value != prev_sensor_value){
-            display("                    ", 1);
-            display(teams[sensor_value],1);
-            prev_sensor_value = sensor_value;
+    unsigned long currentMillis = millis();
+
+    if(currentMillis - previousMillis > interval) {
+        previousMillis = currentMillis;  
+
+        check_wifi();
+        
+        get_live_spread();
+    
+        Serial.println(spread);
+        
+
+        if (spread == "None"){ //If no live game is current available
+            if (game_started == true && spread_prev > 0){
+                announce_win();
+            }
+            game_started = false;
+            interval = 1800000;
+            
+        }else{
+            interval = 10000;
+            announce_islive();
+
+            if (spread.toInt() > spread_prev){
+                announce_goal();
+            }
         }
 
-        if (buttonState == HIGH) {
-            team = teams[sensor_value];
-            display_clear();
-            delay(1000);
+        spread_prev = spread.toInt();
+
+        if (game_started == true){
+            display("Game is on now.", 0);
+            display(String(count), 1);
+        }else{
+            display("Waiting for game.", 0);
+            display(String(count), 1);
         }
 
-    }else{
-        delay(interval);
-        display("Team is:", 0);
-        display(team, 1);
-        if (buttonState == HIGH) {
-            announce_goal();
-        }
-
+        count +=1;
     }
+}    
 
-    check_wifi();
 
-    check_islive();
+void get_live_spread(){
 
     data = http_get(feed_url);
-  
+
     //JSON Parsing Prep
     //Shave start
     int start_index = data.indexOf('[');
     data = data.substring(start_index+1);
 
-    //Clip end of game entry
     int end_index;
     String test_data;
 
@@ -125,67 +139,33 @@ void loop() {
         String away_team = game_result["atn"];
         String home_team = game_result["htn"];
         String game_status = game_result["bs"];
-
+        
         //A given team may have previous and upcoming games listed.  
         //Filter out the live game, get its game id and set home/away
         if (game_started != true) {
             if ((away_team == team) || (home_team == team)){
                 if (game_status == "LIVE"){
-                    Serial.println("game is on now!!");            
-                    current_game_id = game_result["id"];
                     game_started = true;
-                    announce_game_start();
-                    display_clear();
-            //}else if ((game_status != "FINAL") && (game_status != "FINAL OT") && (game_status != "FINAL SO")) {
-                }else{
 
-                    display("                    ",2);
-                    // display(game_status, 2);
-                    display("waiting to start", 3);
+                    if (away_team == team){
+                        team_goals = game_result["ats"];
+                        opponent_goals = game_result["hts"];
+                    }
+                    
+                    if (home_team == team){
+                        team_goals = game_result["hts"];
+                        opponent_goals = game_result["ats"];
+                    }
+                
+                    int num_spread = team_goals - opponent_goals;
+                    spread = String(num_spread);
                 }
-            }
-        }
-        
-        
-        if (current_game_id == game_result["id"]){
-            
-            if (away_team == team){
-                team_goals = game_result["ats"];
-                opponent_goals = game_result["hts"];
-            }
-            
-            if (home_team == team){
-                team_goals = game_result["hts"];
-                opponent_goals = game_result["ats"];
-            }
-            
-            
-            spread = (team_goals - opponent_goals);
-            display("Spread is: ", 2);
-            display(String(spread), 3);
-    
-            if (spread > spread_prev){
-                announce_goal();
-            }
-
-            if (spread > 0){
-                Serial.println("We're ahead!");
-            }
-
-            if ((game_status == "FINAL") || (game_status == "FINAL OT")){
-                if (spread > 0){
-                announce_win();
-                }
-                spread = 0;
-                game_started = false;
-                current_game_id = 0;
-            }
-            spread_prev = spread;
+            }        
         }
         data = data.substring(end_index+2);
-    }  
+    }
 
-      
+
 }
 
 void announce_game_start(){
@@ -193,6 +173,16 @@ void announce_game_start(){
     tone(buzzer, 100, 1000);
     tone(buzzer, 200, 1000);
 }
+
+void announce_start(){
+    Serial.println("Program Started!");
+    tone(buzzer, 100, 100);
+    delay(500);
+    tone(buzzer, 100, 100);
+    delay(500);
+    tone(buzzer, 500, 500);
+}
+
 
 void announce_goal(){
     Serial.println("Goal scored!");
@@ -240,22 +230,13 @@ void check_wifi(){
   }
 }
 
-void check_islive(){
-    if (game_started == true) {
+void announce_islive(){
         digitalWrite(islive_pin, HIGH);
         delay(100);
         digitalWrite(islive_pin, LOW);
 
-    }
 }
-
-
 void display(String chars, int line){
-    // Serial.println(chars);
-    // Serial.println(line);
-
-    // lcd.setCursor(0,line);
-    // lcd.print("                    ");
     lcd.setCursor(0,line);
     lcd.print(chars);
 
